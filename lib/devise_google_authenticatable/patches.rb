2c1f62736cb20d4b4384c3bad63d5b4baea8bee8
Jonathan Mason <jonathan.e.mason@gmail.com>
2014-05-23 15:22:48 -0700

    Get rid of the Registrations controller hack.
    It breaks normal registrations, because it patches all creates.
    If you want to control the redirect location, Devise provides a way.

--- lib/devise_google_authenticatable/patches.rb
+++ lib/devise_google_authenticatable/patches.rb
@@ -1,11 +1,9 @@
 module DeviseGoogleAuthenticator
   module Patches
-    autoload :DisplayQR, 'devise_google_authenticatable/patches/display_qr'
     autoload :CheckGA, 'devise_google_authenticatable/patches/check_ga'
 
     class << self
       def apply
-        Devise::RegistrationsController.send(:include, Patches::DisplayQR)
         Devise::SessionsController.send(:include, Patches::CheckGA)
       end
     end
